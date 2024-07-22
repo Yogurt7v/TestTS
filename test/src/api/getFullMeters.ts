f7b529eaa94a332a4bd652d2ec677cbd106b9cef
Yogurt7v <egorov-2k@yandex.ru>
2024-07-22 18:14:48 +0300

    fix: вернулся к исходному store

--- test/src/api/getFullMeters.ts
+++ test/src/api/getFullMeters.ts
@@ -1,4 +1,4 @@
-import {FullMeters, Meters, Address } from '../types/types';
+import { FullMeters, Meters, Address } from '../types/types';
 
 export function getFullMeters() {
 
@@ -9,10 +9,12 @@ export function getFullMeters() {
   };
 
   const meters = async () =>
-    fetchData("http://showroom.eis24.me/api/v4/test/meters?limit=20");
+    fetchData("http://showroom.eis24.me/api/v4/test/meters");
+
+
 
   const addresses = async () =>
-    fetchData("http://showroom.eis24.me/api/v4/test/areas/");
+    fetchData("http://showroom.eis24.me/api/v4/test/areas");
 
   return Promise.all([meters(), addresses()]).then(
     ([metersList, addressesList]) => {
@@ -21,10 +23,9 @@ export function getFullMeters() {
       metersList.map((obj1: Meters) => {
         const id = obj1.area.id;
         const addressObj = addressesList.find((obj2: Address) => obj2.id === id);
-        const fullMeter : FullMeters = { ...obj1, address: addressObj };
+        const fullMeter: FullMeters = { ...obj1, address: addressObj };
         result.push(fullMeter);
       });
-      
       return result;
     }
   );
